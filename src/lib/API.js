61a35ff671af00dbf27c42200ea1fe3f2a7748af
Marcin Kosmela <martinkosmela@gmail.com>
2019-05-12 21:15:16 +0100

    map added

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,28 +1,32 @@
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const API_KEY = '27ee59722983b17fa4dff6041fc2a975';
-const location = '37.8267,-122.4233';
-const WEATHER_API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/${location}`;
-const GEO_API_URL = `${proxy}https://darksky.net/geo?q=`
+const WEATHER_API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/`;
+const GEO_API_URL = `${proxy}https://darksky.net/geo?q=`;
+const ADDRESS_API_URL = `${proxy}https://darksky.net/rgeo?hires=1`
 
-function getForecast() {
-  // promise format, waiting for data to come back
-  // fetch(API_URL)
-  //   .then(response => response.json())
-  //   .then(result => {
-  //     console.log(result);
-  //   });
-  return fetch(WEATHER_API_URL)
+function getForecast(lat, lng) {
+  return fetch(`${WEATHER_API_URL}${lat},${lng}`)
     .then(response => response.json());  
 }
 
 function getCoordinates(location) {
-  // return fetch(`${GEO_API_URL}${location}`)
-  return fetch(GEO_API_URL + location)
-    .then(response => response.json());  
+  return fetch(`${GEO_API_URL}${location}`)
+    .then(response => response.json());
+}
+
+function getAddress(lat, lng) {
+  return fetch(`${ADDRESS_API_URL}&lat=${lat}&lon=${lng}`)
+    .then(response => response.json());
+}
+
+function getEmbedURL(lat, lng) {
+  return `https://maps.darksky.net/@temperature,${lat},${lng},4?domain=&quot;+encodeURIComponent(window.location.href)+&quot;&amp;auth=1557691511_525fab22d7e25a581110010b386f03dc&amp;embed=true&amp;timeControl=false&amp;fieldControl=false&amp;defaultField=temperature&amp;defaultUnits=_c`;
 }
 
 // export an object which has a function in it
 export default {
+  getAddress,
   getForecast,
-  getCoordinates
+  getCoordinates,
+  getEmbedURL
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,14 @@
         <div class="input-group mb-3">
         <input v-model="location" type="text" class="form-control" placeholder="Location">
         <div class="input-group-append">
-          <button @click="updateLocation" class="btn btn-outline-primary" type="button">Search</button>
+          <button @click="updateLocation" class="btn btn-outline-primary" type="button">
+            Search</button>
         </div>
       </div>
       </div>
       <div v-if="forecast" class="col-8 offset-2 text-center">
         <div class="card text-white bg-secondary mb-3">
-          <div class="card-header">Current weather</div>
+          <div class="card-header">{{address}}</div>
           <div class="card-body">
             <h4 class="card-title">{{forecast.currently.summary}}</h4>
             <p class="card-text icon-temp">
@@ -19,10 +20,19 @@
               {{forecast.currently.temperature}} F
             </p>
             <p class="card-text">
-              {{forecast.currently.precipProbability}} % chance of precipitation
+              {{forecast.currently.precipProbability}} % chance of precipitation.
+              {{forecast.minutely.summary}}
             </p>
           </div>
         </div>
+      </div>
+      <div class="col-8 offset-2" ref="map">
+        <iframe
+          id="map-embed-iframe"
+          frameborder="0"
+          height="500px"
+          width="100%"
+          :src='embedURL'></iframe>
       </div>
     </div>
     <!-- <pre>{{forecast}}</pre> -->
@@ -36,34 +46,47 @@ export default {
   name: 'home',
   data() {
     return {
-      location: '',
+      embedURL: '',
+      location: localStorage.location || '',
+      address: localStorage.address || '',
       forecast: null,
       icons: {
         'clear-day': 'a',
         'clear-night': 'b',
-        'rain': 'c',
-        'snow': 'd',
-        'sleet': 'e',
-        'wind': 'f',
-        'fog': 'g',
-        'cloudy': 'h',
+        rain: 'c',
+        snow: 'd',
+        sleet: 'e',
+        wind: 'f',
+        fog: 'g',
+        cloudy: 'h',
         'partly-cloudy-day': 'i',
-        'partly-cloudy-night' : 'j'
+        'partly-cloudy-night': 'j',
       },
     };
   },
   mounted() {
-    API.getForecast().then(result => {
-      // console.log(result);
-      this.forecast = result;
-    });
+    this.loadWeather(localStorage.lat || '37.8267', localStorage.lng || '-122.4233');
   },
   methods: {
-    updateLocation() {
-      API.getCoordinates(this.location).then(result => {
-        console.log(result);
+    loadWeather(lat, lng) {
+      localStorage.lat = lat;
+      localStorage.lng = lng;
+      this.embedURL = API.getEmbedURL(lat, lng);
+      API.getAddress(lat, lng).then((result) => {
+        this.address = [result.name, result.street].join();
+        localStorage.address = this.address;
       });
-    }
+      API.getForecast(lat, lng).then((result) => {
+        this.forecast = result;
+      });
+    },
+    updateLocation() {
+      localStorage.location = this.location;
+      API.getCoordinates(this.location).then((result) => {
+        // console.log(result.latitude, result.longitude);
+        this.loadWeather(result.latitude, result.longitude);
+      });
+    },
   },
 };
 </script>
